6c5bc6fdbb4f0375b4727bf6f7a6f13728ac8ff3
Juliane Blier <blier.juliane@gmail.com>
2016-11-07 14:06:45 +0100

    utilisation de mustache pour le customer sélectionné

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@
 	var app = {
 		config: {},
 		customers: null,
+		newCustomer: {},
 		init: function(){
 			this.config = window.appConfig;
 			this.listeners();
@@ -10,6 +11,11 @@
 		},
 		listeners: function(){
 			$('#selectCustomer').on('click', this.displayCustomer.bind(this));
+			$('#createCustomer').on('click', this.createCustomer.bind(this));
+			$('form').on('click', function(event){
+				event.preventDefault();
+				return false;
+			});
 		},
 		get: function(path, callback){
 			$.ajax({
@@ -24,15 +30,30 @@
 				$('#clients').append('<option value="' + i + '">' + data.customers[i].first_name + ' ' + data.customers[i].last_name + '</option>');
 			}
 		},
-		displayCustomer: function(){ //A factoriser
+		displayCustomer: function(){
 			var choosenCustomer = this.customers[$('option:selected').val()];
-			$('#first_name').html(choosenCustomer.first_name);
-			$('#last_name').html(choosenCustomer.last_name);
-			$('#role').html(choosenCustomer.role);
-			$('#company').html(choosenCustomer.company);
-			$('#phone').html(choosenCustomer.phone);
-			$('#email').html(choosenCustomer.email);
-			$('#description').html(choosenCustomer.description);
+			var template = '<ul><li>First Name : {{first_name}}</li><li>Last Name : {{last_name}}</li><li>Role : {{role}}</li><li>Company : {{company}}</li><li>Phone : {{phone}}</li><li>Email : {{email}}</li><li>Description : {{description}}</li></ul>';
+			var selectedCustomer = Mustache.to_html(template, choosenCustomer);
+			$('#selectedCustomer').html(selectedCustomer);
+		},
+		createCustomer: function(){
+			this.post('/newCustomer');
+			this.newCustomer = {
+				id: null,
+				first_name: $('#new_first_name').val(),
+				last_name: $('#new_last_name').val(),
+				company: $('#new_company').val(),
+				role: $('#new_role').val(),
+				phone: $('#new_phone').val(),
+				email: $('#new_email').val(),
+				description: $('#new_description').val()
+			}
+		},
+		post: function(path){
+			$.ajax({
+				url: this.config.url + path,
+				type: 'POST'
+			});
 		}
 	}
 

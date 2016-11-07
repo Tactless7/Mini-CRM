82d2b2fff4b8d6ea382687d13893bfc898e6598b
Juliane Blier <blier.juliane@gmail.com>
2016-11-07 10:43:47 +0100

    Affichage du select

--- js/app.js
+++ js/app.js
@@ -2,10 +2,11 @@
 	'use strict';
 	var app = {
 		config: {},
+		customers: null,
 		init: function(){
 			this.config = window.appConfig;
 			this.listeners();
-			this.get('data/crm.json', this.displaySelect.bind(this));
+			this.get('/crm.json', this.displaySelect.bind(this));
 		},
 		listeners: function(){
 			$('select').on('click', 'option', this.displayCustomer.bind(this));
@@ -14,11 +15,16 @@
 			$.ajax({
 				url: this.config.url + path,
 				type: 'GET',
-				success: callback.bind(this)
+				success: callback.bind(this),
 			});
 		},
-		displaySelect: function(){
-
+		displaySelect: function(data){
+			console.log(data);
+			this.customers = data.customers;
+			console.log(this.customers);
+			for (var i = 0 ; i < this.customers.length ; i++) {
+				$('#clients').append('<option value="' + data.customers[i].id + '">' + data.customers[i].first_name + ' ' + data.customers[i].last_name + '</option>');
+			}
 		},
 		displayCustomer: function(){
 
--- js/server.js
+++ js/server.js
@@ -3,8 +3,14 @@ var app = express();
 
 
 
+app.use(function(req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	next();
+});
+
+app.use(express.static('data'));
+
 app.listen(2605, function(){
 	console.log('Gaffe, j\'ecoute tout');
 });
-
-// app.use('/static', express.static('index.html'));
